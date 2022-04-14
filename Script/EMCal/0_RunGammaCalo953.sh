f334b24b573dcac6ce9cd04398c67c863aa51e3f
hanseopark <hapark@cern.ch>
2022-04-14 16:34:57 +0900

    changes made on 2022-04-14T16:34:57

--- Script/EMCal/0_RunGammaCalo953.sh
+++ Script/EMCal/0_RunGammaCalo953.sh
@@ -1,22 +1,22 @@
 # Cluster shape cut
 echo -e "00010113_411790607l032220000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal.txt
 
 echo -e "00010113_411790607l032250000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal.txt
 
 echo -e "00010113_411790607l032220000_2l631031000000d0\n00010113_411790607l032250000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal_953_shape.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal_953_shape.txt
 
 # TM Cut
 echo -e "00010113_411790607e032230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal.txt
 
 echo -e "00010113_411790607g032230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal.txt
 
 echo -e "00010113_411790607h032230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal.txt
 
 echo -e "00010113_411790607e032230000_2l631031000000d0\n00010113_411790607g032230000_2l631031000000d0\n00010113_411790607h032230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < answers_EMCal_953_TM.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_953.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_953.root eps  < Answers/answers_EMCal_953_TM.txt
--- Script/EMCal/0_RunGammaCalo961.sh
+++ Script/EMCal/0_RunGammaCalo961.sh
@@ -1,35 +1,35 @@
 # Energy Calibration
 echo -e "0008d113_411790607l022230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961.txt
 
 echo -e "0008d113_411790607l042230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961.txt
 
 echo -e "0008d113_411790607l052230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961.txt
 
 # Energy cluster cut
 #echo -e "0008d113_411790607l022230000_2l631031000000d0\n0008d113_411790607l042230000_2l631031000000d0\n0008d113_411790607l052230000_2l631031000000d0" > CutSelection.log
-#    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961_energy.txt
+#    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961_energy.txt
 echo -e "0008d113_411790607l022230000_2l631031000000d0\n0008d113_411790607l042230000_2l631031000000d0\n0008d113_411790607l052230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961_energy.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961_energy.txt
 
 # NCell cut
 #echo -e "0008d113_411790607l022230000_2l631031000000d0" > CutSelection.log
-#    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961.txt
+#    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961.txt
 
 echo -e "0008d113_411790607l031230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961.txt
 
 echo -e "0008d113_411790607l033230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -gammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961.txt
 
 echo -e "0008d113_411790607l031230000_2l631031000000d0\n0008d113_411790607l033230000_2l631031000000d0" > CutSelection.log
-    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < answers_EMCal_961_ncells.txt
+    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff /home/alidock/alice/work/Data/pp_13TeV/Injet/DataMerged/GammaCalo_961.root /home/alidock/alice/work/Data/pp_13TeV/Injet/MCMerged/GammaCalo_961.root eps  < Answers/answers_EMCal_961_ncells.txt
 
 
 # # Alpha
 #echo -e "80000113_00200009327000008250400000_2444451041013200000_0163103100000010\n80000113_00200009327000008250400000_2444451041013200000_0163105100000010\n80000113_00200009327000008250400000_2444451041013200000_0163106100000010" > CutSelectionAlpha.log
-#    echo -e "Alpha\nLHC13bc\n3\nY\npPb5\nY\n/home/mike/2_pPb_EMC/0_analysis/170803_final_EMC/CocktailEMC_4Mio.root\n0.80\nN\nY\nY" > answersAlpha.txt
+#    echo -e "Alpha\nLHC13bc\n3\nY\npPb5\nY\n/home/mike/2_pPb_EMC/0_analysis/170803_final_EMC/CocktailEMC_4Mio.root\n0.80\nN\nY\nY" > Answers/answersAlpha.txt
 #    cp CutSelectionAlpha.log CutSelection.log
-#    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff bla eps < answersAlpha.txt
+#    bash start_FullMesonAnalysis_TaskV3.sh -dgammaOff bla eps < Answers/answersAlpha.txt
